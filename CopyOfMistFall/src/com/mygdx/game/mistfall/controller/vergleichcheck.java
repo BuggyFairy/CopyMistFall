39ecb6df67cd888c8477253164c806895ef7ddfe
NSA <topsecret@nsa.com>
2016-04-18 14:10:50 +0200

    vergleichcheck erweitert

--- CopyOfMistFall/src/com/mygdx/game/mistfall/controller/vergleichcheck.java
+++ CopyOfMistFall/src/com/mygdx/game/mistfall/controller/vergleichcheck.java
@@ -21,5 +21,8 @@ public class vergleichcheck {
 			System.out.println(enemies.get(0).equals(v2)); //Ist v1 gleich v2 ?
 			System.out.println(enemies.get(1).equals(v1)); //Ist v2 gleich v1 ?
 			System.out.println(enemies.get(1).equals(v2)); //Ist v2 gleich v2 ?
+			VampireBatSwarm v3 = v1; //v3 ist nun kopie von v1
+			enemies.add(v3); //Ich bin an Stelle 2 in der Liste
+			System.out.println(enemies.get(2).equals(v1)); //Ist v3 gleich v1 ?
 		}
 }
